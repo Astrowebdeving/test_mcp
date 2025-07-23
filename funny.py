9669366c5c05a811f15cc39b29941fcc8a258902
AstroWebDev <94124964+Astrowebdeving@users.noreply.github.com>
2025-07-23 16:06:17 -0500

    u

--- funny.py
+++ funny.py
@@ -1,4 +1,5 @@
 import random 
 #smallchanges
+#did_a_funy
 def number_generator(int a, int b) -> int: 
-  return a*random.randint(a,b)*b
+  return (a*random.randint(a,b)*b - random.randint(a,b)*a*a*b*b)
