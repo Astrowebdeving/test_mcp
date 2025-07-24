b02ec7702abcacb596b3c34564eca5410e6ab794
AstroWebDev <94124964+Astrowebdeving@users.noreply.github.com>
2025-07-24 14:59:04 -0500

    ao

--- funny.py
+++ funny.py
@@ -3,3 +3,6 @@ import random
 #did_a_funy
 def number_generator(int a, int b) -> int: 
   return (a*random.randint(a,b)*b - random.randint(a,b)*a*a*b*b)
+
+def number_power_silly(int a, in b) -> int: 
+    return (a*random.randint(a,b)*a + b*b*random.randint(a,b))
